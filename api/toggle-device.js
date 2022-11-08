399f9676204eb0f4179d0d25d7e30d359aa29710
Massimiliano Marcon <massimiliano.marcon@mongodb.com>
2022-11-08 17:53:19 +0100

    Does it work?

--- api/toggle-device.js
+++ api/toggle-device.js
@@ -1,7 +1,20 @@
-export default function handler(request, response) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
-}
+const { MongoClient } = require('mongodb');
+const fetch = require('node-fetch');
+const https = require('https');
+
+let client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const clientPromise = client.connect();
+
+export default async function handler(req, res) {
+  client = await clientPromise;
+  const collection = client.db('Building').collection('clients');
+  const client = await collection.findOne({name: 'myclient1'});
+  try {
+    
+  }
+  catch (e) {
+    console.error(e);
+  }
+
+  res.send(`Hello ${client.name}`);
+};
