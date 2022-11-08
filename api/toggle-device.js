2af4c549171adf9a6309624d76a88a1973bc4d09
Massimiliano Marcon <massimiliano.marcon@mongodb.com>
2022-11-08 18:02:47 +0100

    Boom

--- api/toggle-device.js
+++ api/toggle-device.js
@@ -9,8 +9,27 @@ export default async function handler(req, res) {
   client = await clientPromise;
   const collection = client.db('Building').collection('clients');
   const clientInfo = await collection.findOne({name: 'myclient1'});
+
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
+    cert: clientInfo.cert,
+    key: clientInfo.key});
+
+  const raw = JSON.stringify({
+    "@type": "powerSwitchState",
+    "switchState": "OFF"
+  });
+
+  const requestOptions = {
+    method: 'PUT',
+    headers: {"Content-Type": "application/json"},
+    body: raw,
+    redirect: 'follow',
+    agent: httpsAgent
+  };
+
   try {
-    
+    await fetch(`https://${clientInfo.hostport}/smarthome/devices/hdm%3AHomeMaticIP%3A3014F711A00004953859E5E2/services/PowerSwitch/state`, requestOptions);
   }
   catch (e) {
     console.error(e);
